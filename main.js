b6cb310ffeaf938a89b80246edb93ce24c44e587
Advita-Ishi <80190120+Advita-Ishi@users.noreply.github.com>
2022-01-15 17:05:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+Webcam.set({
+    width:300,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+    camera = document.getElementById("cam");
+    Webcam.attach(camera);
+
+    function capture() {
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        });
+    }
+    console.log("ml5 version is",ml5.version);
+
+    var classifier = ml5.imageClassifier("//link",modelloaded);
+    function modelloaded() {
+        console.log("model is loaded");
+    }
